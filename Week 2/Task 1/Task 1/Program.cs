ad1ee75437a3e30e25dace9d0717a72ffff532ac
RakhatAubakirov <rakhat.a.2014@mail.ru>
2019-02-08 00:26:35 +0600

    lab2

---
+++ Week 2/Task 1/Task 1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileStream fs = new FileStream(@"D:\Desktop\PP2\Week 2\inf.txt" , FileMode.Open , FileAccess.Read);
+            StreamReader sr = new StreamReader(fs);
+            string s = sr.ReadLine();
+            char[] arr = s.ToCharArray();
+            Array.Reverse(arr);
+            string s1 = new string(arr);
+            if (s == s1)
+                Console.WriteLine("Yes");
+            else
+                Console.WriteLine("No");
+        }
+    }
+}
