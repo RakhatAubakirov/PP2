529c6c3b2cb063685c006c115f2f1b23bb7d7332
RakhatAubakirov <rakhat.a.2014@mail.ru>
2019-02-10 20:52:33 +0600

    lab2

---
+++ Week 2/Task 2/Task 2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool IsPrime(int x)
+            {
+                int cnt = 0;
+                for(int i = 1; i <= x; i++)
+                {
+                    if (x % i == 0)
+                        cnt++;
+                }
+                if (cnt == 2)
+                    return true;
+                return false;
+            }
+            StreamReader sr = new StreamReader(@"D:\Desktop\PP2\Week 2\info.txt");
+            StreamWriter sw = new StreamWriter(@"D:\Desktop\PP2\Week 2\answer.txt");
+            string[] arr = sr.ReadLine().Split(' ');
+            for(int i = 0; i < arr.Length; i++)
+            {
+                int x = int.Parse(arr[i]);
+                if (IsPrime(x))
+                    sw.Write(x + " ");
+
+            }
+            sr.Close();
+            sw.Close();
+
+           
+        }
+    }
+}
---
+++ Week 2/Task 3/Task 3/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_3
+{
+    class Program
+    {
+        static void Print(int lvl)
+        {
+            for (int i = 0; i < lvl; i++)
+                Console.Write(" ");
+        }
+
+            static void ex3(DirectoryInfo directory , int lvl)
+        {
+            FileInfo[] files = directory.GetFiles();
+            DirectoryInfo[] directories = directory.GetDirectories();
+
+                foreach(FileInfo file in files)
+            {
+                Print(lvl);
+                Console.WriteLine(file.FullName);
+            }
+                foreach(DirectoryInfo dir in directories)
+            {
+                Print(lvl);
+                Console.WriteLine(dir.FullName);
+                ex3(dir, lvl + 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            DirectoryInfo dir = new DirectoryInfo(@"D:\Desktop\PP2\Week 2");
+            ex3(dir, 0);
+
+        }
+    }
+}
