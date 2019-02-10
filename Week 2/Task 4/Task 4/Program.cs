48ebe4d21bd99d171a72faa1ff248b3339131b1c
RakhatAubakirov <rakhat.a.2014@mail.ru>
2019-02-10 21:09:40 +0600

    task

---
+++ Week 2/Task 4/Task 4/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Path1 = @"D:\Desktop\PP2\Week 2\file 1.txt";
+            string Path2 = @"D:\Desktop\PP2\Week 2\file 2.txt";
+            StreamWriter sw = new StreamWriter(Path1);
+            sw.WriteLine("something");
+            sw.Close();
+            File.Copy(Path1, Path2);
+            File.Delete(Path1);
+        }
+    }
+}
